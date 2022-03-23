cf6a087e8720bd6bb8cd4d8c92381e64cb58c4e5
hendriprst <hendriprastio99@gmail.com>
2022-03-23 13:01:34 +0700

    add single-sample and edit App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './single-sample';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={data.album.images[1].url} className="App-logo" alt="" />
+        <h3 className="App-title">{data.name}</h3>
+        <p className="App-artist">{data.artists[0].name}</p>
+        <button>Select</button>
       </header>
     </div>
   );
