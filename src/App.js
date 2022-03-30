69147955e8951f58e162c9ac03afb5fde2abc2dd
hendriprst <hendriprastio99@gmail.com>
2022-03-30 12:03:21 +0700

    auth spotify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import Playlist from './components/Playlist';
 import AllSample from './assets/data/AllSample';
+import GetPlaylist from './components/GetPlaylist';
+
+const TrackLoop = () => {
+  const AllList = AllSample.map((a) => (
+    <Playlist 
+      images={a.album.images[1].url}
+      title={a.album.name}
+      artist={a.album.artists[0].name}
+      albums={a.name}
+    />
+  ));
+  return AllList;
+}
 
 function App() {
-
-  const TrackLoop = () => {
-    const AllList = AllSample.map((a) => (
-      <Playlist 
-        images={a.album.images[1].url}
-        title={a.album.name}
-        artist={a.album.artists[0].name}
-        albums={a.name}
-      />
-    ));
-    return AllList;
-  }
   return (
     <div className="App">
       <header className="App-header">
         <div className="card">
+          <GetPlaylist />
           <TrackLoop />
         </div>
       </header>
---
+++ src/components/GetPlaylist.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function GetPlaylist() {
+  return <button type="submit" className="btn btn_get">Get Spotifi Playlist</button>
+}
+
+export default GetPlaylist;
