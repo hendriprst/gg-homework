edecb9447fb219532539b8e42b7906d0e56bc5d7
hendriprst <hendriprastio99@gmail.com>
2022-04-04 04:59:45 +0700

    create playlist

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Playlist from './components/album/Playlist';
 import GetPlaylist from './components/searchBar/GetPlaylist';
-
-// const TrackLoop = () => {
-//   const AllList = AllSample.map((a) => (
-//     <Playlist 
-//       images={a.album.images[1].url}
-//       title={a.album.name}
-//       artist={a.album.artists[0].name}
-//       albums={a.name}
-//     />
-//   ));
-//   return AllList;
-// }
 
 const App = () => {
   const [ data, setData ] = useState([]);
   const [ query, setQuery ] = useState("bohemian");
+  const [ selected, setSelected ] = useState([]);
 
   const handleSearch = (value) => setQuery(value);
 
   useEffect(() => {
-    axios(`https://api.spotify.com/v1/search?q=${query}&type=artist`)
+    fetch(`https://api.spotify.com/v1/search?q=${query}&type=artist`)
     .then((res) => setData(res.data))
     .catch((err) => console.log(err))
   }, [query]);
--- src/components/album/Playlist.jsx
+++ src/components/album/Playlist.jsx
@@ -9,7 +9,7 @@ const Playlist = ({ images, title, artist, albums }) => {
       <h3 className="card-title">{title}</h3>
       <p className="card-artist">Artist: {artist}</p>
       <p className="card-album">Album: {albums}</p>
-      <button href="#" className="btn btn_select">Select</button>
+      <button type="submit" className="btn btn_select">Select</button>
     </div>
     </>
   );
